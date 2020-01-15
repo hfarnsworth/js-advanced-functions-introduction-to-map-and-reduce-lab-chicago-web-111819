8e345a7a8c99e25d49b91addec0d87c52b10d1f8
hfarnsworth <havilahfarnsworth@gmail.com>
2020-01-15 17:20:23 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // Your code here
+function mapToNegativize(arr) {
+    return arr.map( n => n * -1 )
+}
+
+function mapToNoChange(arr) {
+    return arr.map( n => n )
+}
+
+function mapToDouble(arr) {
+    return arr.map( n => n * 2 )
+}
+
+function mapToSquare(arr) {
+    return arr.map( n => n ** 2 )
+}
+
+function reduceToTotal(arr, start = 0) {
+    let total = start
+    for (let i = 0; i < arr.length; i++) {
+        total = arr[i] + total
+    }
+    return total
+}
+
+function reduceToAllTrue(arr) {
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] == false) {
+            return false
+        }
+    }
+    return true
+}
+
+function reduceToAnyTrue(arr) {
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] == true) {
+            return true
+        }
+    }
+    return false
+}
